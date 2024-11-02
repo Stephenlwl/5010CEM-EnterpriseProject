a175eba8117c2c36eee5a379a65a06100724acd9
stephen <stephen@globalconnect.com.my>
2024-11-02 22:02:38 +0800

    fix on nav bug on report menagement

--- public/admin/generateReport.php
+++ public/admin/generateReport.php
@@ -39,6 +39,8 @@ $inventoryData = $inventoryStmt->fetchAll(PDO::FETCH_ASSOC);
     <title>Daily Report</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="css/navigation.css">
     <link rel="stylesheet" href="css/adminPublicDefault.css">
     <script src="js/generateReport.js"></script>
 </head>
